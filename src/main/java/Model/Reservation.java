a91bbcb67229964e2f22749883af861022026d59
pankeny <konrad_malocha@hotmail.com>
2018-11-08 23:38:00 +0100

    add Reservation class

---
+++ src/main/java/Model/Reservation.java
@@ -0,0 +1,61 @@
+package Model;
+
+import java.util.Date;
+
+public class Reservation {
+
+    private Integer reservationId;
+    private Integer roomNubmer;
+    private Integer clientId;
+    private Date startDate;
+    private Date endDate;
+    private Double AmountDue;
+
+    public Integer getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(Integer reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public Integer getRoomNubmer() {
+        return roomNubmer;
+    }
+
+    public void setRoomNubmer(Integer roomNubmer) {
+        this.roomNubmer = roomNubmer;
+    }
+
+    public Integer getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Integer clientId) {
+        this.clientId = clientId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Double getAmountDue() {
+        return AmountDue;
+    }
+
+    public void setAmountDue(Double amountDue) {
+        AmountDue = amountDue;
+    }
+}

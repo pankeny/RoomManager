ca8ce1c24e2e1149af3c0ec78a691b8d107e2803
pankeny <konrad_malocha@hotmail.com>
2018-12-18 23:20:36 +0100

    remove ssl warn

--- src/main/java/io/github/pankeny/controller/DatabaseController.java
+++ src/main/java/io/github/pankeny/controller/DatabaseController.java
@@ -20,7 +20,7 @@ public class DatabaseController {
 
     private Connection getConnection() throws SQLException{
 
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/roomsmanager?useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "Test123.");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/roomsmanager?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "Test123.");
 
     }
 
